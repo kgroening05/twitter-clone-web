64af04fd2005c8a1c22bc4ea197f3e83393c0007
Kyle Groening <kylegroening@gmail.com>
2023-03-12 18:08:55 -0400

    Remove boilerplate create-react-app code

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import AddTweetForm from '../components/AddTweetForm';
+import LoginForm from '../components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* add navbar */}
       </header>
+      <main>
+        <AddTweetForm />
+        <LoginForm />
+        <Outlet />
+
+      </main>
     </div>
   );
 }
