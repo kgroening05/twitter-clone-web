b502d4f8859630710a8f6de1c83c7691d7629524
Kyle Groening <kyle.groening@cytiva.com>
2023-03-12 19:43:53 -0400

    Add p and h2 elements

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom"
 import { googleAuth } from "../utils.js/apiCalls"
+import './LoginForm.css'
 export default function LoginForm() {
   return (
     <section className="login">
+        <h2>New to Twitter?</h2>
+        <p>Sign up now to get your own personalized timeline!</p>
         <Link to={googleAuth}><button>Sign In With Google</button></Link>
         <Link to={'/'}><button>Sign In With Apple</button></Link>
         <Link to={'/'}><button>Sign In With Github</button></Link>
         <Link to={'/'}><button>Create Account</button></Link>
+        <p>
+          By signing up, you agree to the <a href="">Terms of Service</a> and <a href="">Privacy Policy</a>
+          , including <a href="">Cookie Use</a>.
+        </p>
     </section>
   )
 }
