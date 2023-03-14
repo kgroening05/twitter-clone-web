530ec62b7b21761f5468ac4240bf3499e0246d72
Kyle Groening <kyle.groening@cytiva.com>
2023-03-14 15:45:50 -0400

    Move author above body

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -3,8 +3,8 @@ export default function FeedCard({ body, author, id }) {
     console.log(author)
     return(
         <div className="tweet-card" key={id}>
-          <p>{body}</p>
           <p>{author}</p>
+          <p>{body}</p>
         </div>
     )
 }
