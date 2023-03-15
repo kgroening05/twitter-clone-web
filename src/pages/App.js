1d8d9fcfc6eff53f0bbe9ff3d6b6bf69e8fead0e
Kyle Groening <kylegroening@gmail.com>
2023-03-14 20:50:06 -0400

    Add useEffect hook to load user data into state

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import AddTweetForm from '../components/AddTweetForm';
 import LoginForm from '../components/LoginForm';
 
 function App() {
+
+  const [ user, setUser ] = useState('user');
+
+  useEffect(() => {
+    console.log('fetching')
+    fetch(`http://localhost:3001/api/login/token-check`,{ credentials: 'include' })
+    .then(response => response.json())
+    .then(result => {
+      setUser(result)
+    })
+  
+  }, [  ])
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* add navbar */}
+      
       </header>
       <main>
+        <p>{user.email}</p>
+        <a href="http://localhost:3001/api/login/token-check">Get User</a>
         <AddTweetForm />
         <LoginForm />
         <Outlet />
