b9b6c1727d2d5052a71ce22864552de80a25ef61
Kyle Groening <kylegroening@gmail.com>
2023-03-21 20:41:11 -0400

    Remove console.log, add profile pic

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -1,10 +1,10 @@
 
-export default function FeedCard({ body, author, id }) {
-    console.log(author)
+export default function FeedCard({ body, author, id, profilePic }) {
     return(
         <div className="tweet-card" key={id}>
           <p>{author}</p>
           <p>{body}</p>
+          <img src={profilePic} alt="" />
         </div>
     )
 }
