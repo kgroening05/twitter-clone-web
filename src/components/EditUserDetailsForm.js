d943a71e30d8c23889afcaef17a94dc16b8d8434
Kyle Groening <kylegroening@gmail.com>
2023-03-21 20:40:13 -0400

    Add new component to update user info

---
+++ src/components/EditUserDetailsForm.js
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
+
+export default function EditUserDetailsForm({ user }){
+
+    const [ username, setUsername ] = useState(user.username);
+    const [ email, setEmail ] = useState(user.email);
+    
+    async function useUpdateUsers(event){
+        event.preventDefault();
+        let navigate = useNavigate();
+        const username = event.target.username.value
+        const email = event.target.email.value
+        const update = await axios.put(
+            `api/user/`,
+            {username, email},
+        )
+        if(update.data.success){
+            console.log('success')
+            navigate('/')
+        }
+    }
+    
+    return(
+        <form onSubmit={ useUpdateUsers }>
+            <label htmlFor="username">Username:</label>
+            <input type="text" 
+                id="username" 
+                name="username" 
+                value={username} 
+                onChange={e => setUsername(e.target.value)}
+            />
+            <label htmlFor="email">Email:</label>
+            <input type="text"
+                id="email"
+                name="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
