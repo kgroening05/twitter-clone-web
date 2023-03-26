99b2b956780bd6a8c3e50ce74bfa93b7726a544f
Kyle Groening <kylegroening@gmail.com>
2023-03-26 16:25:35 -0400

    Add submit handlers to corrently update tweet feed

--- src/components/AddTweetForm.js
+++ src/components/AddTweetForm.js
@@ -1,10 +1,36 @@
 import { addTweet } from "../utils.js/apiCalls"
+import { useState } from "react"
+import './AddTweetForm.css'
 
-export default function AddTweetForm(){
+export default function AddTweetForm({ user, updateTweets, setUpdateTweets }){
+
+    const [ tweetTest, setTweetText ] = useState('')
+
+    const profileDivStyles = {
+        background: `center / contain no-repeat url(${user.profilePic})`,
+      }
+
+    async function handleSubmit(e) {
+        const res = await addTweet(e)
+        if (res === 1) {
+            setUpdateTweets(updateTweets + 1)
+            setTweetText('')
+        }
+    }
+
     return (
-        <form onSubmit={ addTweet }>
-            <input type="text" name="tweet" placeholder="What's happening?"/>
-            <button type="submit">Tweet</button>
-        </form>
+        <div className="add-tweet">
+            <div style={profileDivStyles} className="profile-pic" alt="" ></div>
+            <form onSubmit={ e=>handleSubmit(e) } className="add-tweet-form">
+                <input 
+                    type="text" 
+                    name="tweet" 
+                    placeholder="What's happening?"
+                    value={tweetTest} 
+                    onChange={e => setTweetText(e.target.value)}
+                />    
+                <button type="submit">Tweet</button>
+            </form>
+        </div>
     )
 }
