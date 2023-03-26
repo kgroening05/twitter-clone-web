d238e5e512a7d5909bfe94ce890c7efd4d1db80a
Kyle Groening <kylegroening@gmail.com>
2023-03-26 16:26:36 -0400

    Add styles and classnames, profile pic

--- src/components/FeedCard.js
+++ src/components/FeedCard.js
@@ -1,10 +1,17 @@
+import './FeedCard.css'
 
 export default function FeedCard({ body, author, id, profilePic }) {
+  const profileDivStyles = {
+    background: `center / contain no-repeat url(${profilePic})`,
+  }
+
     return(
         <div className="tweet-card" key={id}>
-          <p>{author}</p>
-          <p>{body}</p>
-          <img src={profilePic} alt="" />
+          <div style={profileDivStyles} className="profile-pic" alt="" ></div>
+          <div className="tweet-text">
+            <p className='author'>{author}</p>
+            <p className='tweet'>{body}</p>
+          </div>
         </div>
     )
 }
